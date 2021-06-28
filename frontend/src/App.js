4af72e791adf4dc1616cf1af9aed4541ecac93c1
Marek <emhala8@gmail.com>
2021-06-28 22:45:34 +0200

    created card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import { Switch } from 'react-router-dom';
+import "./App.css";
+import { Switch } from "react-router-dom";
+import Card from "./components/Card";
 
 function App() {
   return (
     <Switch>
-      <div></div>
+      <Card
+        title="Dog Shelter"
+        imageUrl="https://media.4-paws.org/a/9/b/d/a9bd2520c2a7c941680bbfc56182ed5615e7cd3c/VIER%20PFOTEN_2016-09-18_081-1927x1333.jpg"
+        body="Donate now to fund animal rescue organizations, provide affordable equipment for wildlife search and rescue, and support adoptable cats, dogs, and other pets ...        "
+      />
     </Switch>
   );
 }
---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./card.css";
+
+function Card ({title, imageUrl, body}) {
+    return (
+        <div className="card-container">
+            <div className="image-container">
+                <img src={imageUrl} alt="" />
+            </div>
+            <div className="card-content">
+            <div className="card-title">
+               <h3> {title}</h3>
+            </div>
+            <div className="card-body">
+                <p>{body}</p>
+            </div>
+            </div>
+            <div className="donation-progress">
+
+            <div class="box">
+            <h2 class="text">Donation progress</h2>
+  <div class="percent">
+    <svg>
+      <circle cx="70" cy="70" r="70"></circle>
+      <circle cx="70" cy="70" r="70"></circle>
+    </svg>
+    <div class="num">
+      <h2>80<span>%</span></h2>
+    </div>
+  </div>
+</div>
+            </div>
+            <div className="button-container">
+                <button className="button"><a>View more</a></button>
+                <button className="button"><a>Donate</a></button>
+            </div>
+        </div>
+    )
+}
+
+export default Card
