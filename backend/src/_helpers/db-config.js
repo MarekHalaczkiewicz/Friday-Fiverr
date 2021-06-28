94e15356454845c316a1082f6462a504401ca649
Luca <luca.milanese90@gmail.com>
2021-06-28 18:18:25 +0200

    added routes to create and authenticate users

---
+++ backend/src/_helpers/db-config.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const dbPath = process.env.DB_LINK;
+
+const connectionOptions = {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
+mongoose.connect(dbPath, connectionOptions);
+mongoose.Promise = global.Promise;
+
+module.exports = {
+  User: require('../models/user-model'),
+};
