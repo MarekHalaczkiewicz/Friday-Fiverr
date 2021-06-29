5658ddb47dc30954ad5f18d3cbbbfd9e2ea21786
Marek <emhala8@gmail.com>
2021-06-29 14:33:24 +0200

    added to Modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import './App.css';
-import { Switch } from 'react-router-dom';
-import { useState } from 'react';
-import UserContext from './UserContext';
-import Card from './components/Card';
-import Modal from './components/Modal';
+import "./App.css";
+import { Switch } from "react-router-dom";
+import { useState } from "react";
+import UserContext from "./UserContext";
+import Card from "./components/Card";
+import Modal from "./components/Modal";
 
 const projects = [
   {
-    title: 'Dog Shelter',
+    title: "Dog Shelter",
     media: [
-      'https://media.4-paws.org/a/9/b/d/a9bd2520c2a7c941680bbfc56182ed5615e7cd3c/VIER%20PFOTEN_2016-09-18_081-1927x1333.jpg',
+      "https://media.4-paws.org/a/9/b/d/a9bd2520c2a7c941680bbfc56182ed5615e7cd3c/VIER%20PFOTEN_2016-09-18_081-1927x1333.jpg",
     ],
-    body: 'Donate now to fund animal rescue organizations, provide affordable equipment for wildlife search and rescue, and support adoptable cats, dogs, and other pets...',
+    body: "Donate now to fund animal rescue organizations, provide affordable equipment for wildlife search and rescue, and support adoptable cats, dogs, and other pets. We also looking for people, who can create a short video for us, to be played in local tv",
     goal: 500,
-    contributions: [{ amount: 5, id: 'adsfasfsadfda' }],
-    skills: [],
-    tags: [],
-    location: 'Berlin',
-    organization: 'Dog Shelter Berlin',
+    contributions: [{ amount: 5, id: "adsfasfsadfda" }],
+    skills: ["Video Editing", "Short video Ads", "Camera"],
+    tags: ["Filming", "Editing"],
+    location: "Berlin",
+    organization: "Dog Shelter Berlin",
   },
 ];
 
@@ -25,9 +25,9 @@ function App() {
   const [modalOpen, setModalOpen] = useState(false);
   const [projectList, setProjectList] = useState(projects);
   const [user, setUser] = useState({
-    username: '',
+    username: "",
     isLoggedIn: false,
-    userId: '',
+    userId: "",
   });
 
   return (
