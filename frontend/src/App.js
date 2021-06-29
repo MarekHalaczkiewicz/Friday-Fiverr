f0042043dab4fa075c24c10f9503e88e302f0515
Marek <emhala8@gmail.com>
2021-06-29 11:38:37 +0200

    Created a Modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
 import "./App.css";
+import React, { useState } from "react";
 import { Switch } from "react-router-dom";
-
+import Modal from "./components/Modal";
 import Card from "./components/Card";
 
-
-function App() {
+function App(props) {
+  const [modalOpen, setModalOpen] = useState(true);
   return (
-    <Switch>
-
+    <div className="App">
       <Card
+        setOpenModal={setModalOpen}
         title="Dog Shelter"
         imageUrl="https://media.4-paws.org/a/9/b/d/a9bd2520c2a7c941680bbfc56182ed5615e7cd3c/VIER%20PFOTEN_2016-09-18_081-1927x1333.jpg"
         body="Donate now to fund animal rescue organizations, provide affordable equipment for wildlife search and rescue, and support adoptable cats, dogs, and other pets ...        "
       />
-
-    </Switch>
+      {modalOpen && <Modal setOpenModal={setModalOpen} />}
+    </div>
   );
 }
 
