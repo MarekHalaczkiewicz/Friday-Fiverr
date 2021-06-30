20d2bd8d3eecb871c5ce8ed8145d2b9f9832e6c5
Zahra <zahra@zahrasaid.org>
2021-06-30 11:03:56 +0200

    Inputs for Take Part component

--- frontend/src/components/Subscription.js
+++ frontend/src/components/Subscription.js
@@ -5,14 +5,53 @@ export const Subscription = () => {
   const [value, setValue] = useState("");
   return (
     <div>
-      <h1>I want to work on this project</h1>
-      <textarea
-        className="textarea"
-        placeholder="0/200"
-        maxLength="200"
-        onChange={(event) => setValue(event.target.value)}
-        value={value}
-      ></textarea>
+      <form>
+        <h1>I want to work on this project</h1>
+        <label>
+          Name:
+          <input
+            type="text"
+            placeholder="Your Name"
+            // value={this.state.value}
+            // onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Fiverr:
+          <input
+            type="text"
+            placeholder="Your Fiverr URL"
+            // value={this.state.value}
+            // onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Skillset:
+          <input
+            type="text"
+            placeholder="Name project relevant skills"
+            // value={this.state.value}
+            // onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Pitch:
+          <input
+            type="text"
+            placeholder="Why would you like the job?"
+            // value={this.state.value}
+            // onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+        {/* <textarea
+          className="textarea"
+          placeholder="0/200"
+          maxLength="200"
+          onChange={(event) => setValue(event.target.value)}
+          value={value}
+        ></textarea> */}
+      </form>
     </div>
   );
 };
